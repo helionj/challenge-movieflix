e51c0745917e7e0eff8124267b93b063b9422cfa
helionporto <helionporto@hotmail.com>
2021-07-23 19:17:19 -0400

    Home styling

---
+++ frontend/src/pages/Home/Login/index.tsx
@@ -0,0 +1,33 @@
+import ButtonIcon from 'components/ButtonIcon';
+import './styles.css';
+
+const Login = () => {
+  return (
+    <div className="login-card base-card">
+      <h1>LOGIN</h1>
+      <form>
+        <div className="mb-4">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
