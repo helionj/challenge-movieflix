e148f80980af2a3c5e98643c64227dfed8a48897
helionporto <helionporto@hotmail.com>
2021-07-27 10:32:18 -0400

    Logout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,54 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getTokenData, isAuthenticated, TokenData } from 'util/auth';
+import history from 'util/history';
+import { removeAuthData } from 'util/storage';
 import './styles.css';
+
+type AuthData = {
+  tokenData?: TokenData;
+  authenticated: boolean;
+};
+
 const Navbar = () => {
+  const [authData, setAuthData] = useState<AuthData>({ authenticated: false });
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthData({
+        authenticated: false,
+      });
+    }
+  },[]);
+
+  const handleClick = (event:React.MouseEvent<HTMLButtonElement> ) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthData({
+      authenticated: false,
+    });
+    history.replace('/')
+  }
   return (
     <nav className="navbar main-nav bg-primary">
       <div className="container-fluid">
         <Link className="navbar-logo-text" to="/">
           <h4>MovieFlix</h4>
         </Link>
+        <div className= "logout-container">
+          {authData.authenticated && (
+            <button className="btn" onClick={handleClick}>
+              <h5>SAIR</h5>
+            </button>
+            
+          ) }
+        </div>
       </div>
     </nav>
   );
